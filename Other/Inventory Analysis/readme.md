c56eaa849d9778474416f5e49cb8fe860e722ce3
Ramdhan Hidayat <ramdhan_hidayat@outlook.com>
2023-08-02 22:37:54 +0700

    priceopt eda updates

---
+++ Other/Inventory Analysis/readme.md
@@ -0,0 +1,3 @@
+# Inventory Analysis
+
+**Project status**: On Hold, waiting for update from dataset publisher regarding variable descriptions
---
+++ price_optimization/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project analyzes A/B testing results for Company XYZ to optimize product pricing. The analysis explores user behavior, determines conversion rates for different pricing strategies, and aims to identify the optimal price for revenue maximization. The goal is to provide data-driven insights to shape the company's pricing and marketing strategies.',
+    author='Ramdhan Hidayat',
+    license='MIT',
+)
